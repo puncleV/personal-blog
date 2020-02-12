46e1fbc43f86a3bb6c3e7739317e4f91f9e584f4
punkkk <punkrus18@gmail.com>
2020-02-12 16:54:04 +0300

    add: app header

---
+++ src/components/app-header.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    <div>1</div>
+    <div>2</div>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "app-header"
+    }
+</script>
+
+<style scoped>
+
+</style>
