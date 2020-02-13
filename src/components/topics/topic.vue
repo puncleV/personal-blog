463d6615bab1ed0bec0a777c9aca4a760cfd2346
punkkk <punkrus18@gmail.com>
2020-02-13 15:43:50 +0300

    add: navigation to a topic page

---
+++ src/components/topics/topic.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>title</h1>
+        <div></div>
+        <div></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "topic"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import About from '../components/about'
 import TopicsList from '../components/topics/topics-list'
+import Topic from '../components/topics/topic'
 import Home from '../components/home'
 
 Vue.use(Router)
@@ -17,6 +18,11 @@ export default new Router({
       path: '/blog',
       name: 'Blog',
       component: TopicsList
+    },
+    {
+      path: '/blog/:id',
+      name: 'Topic',
+      component: Topic
     },
     {
       path: '/',
