d4afe76382f0062c6303d202c7e9c065ce914c94
punkkk <punkrus18@gmail.com>
2020-02-13 12:17:50 +0300

    add: header navigation

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,14 +1,39 @@
 <template>
-<div>
-    <div>Home</div>
-    <div>Blog</div>
-    <div>About</div>
-</div>
+    <header>
+        <div
+                v-for="element in elements"
+                @click="$router.push({
+                    name: element.routeName
+                })"
+                v-bind:class="{
+                    active: $router.currentRoute.name === element.routeName
+                }"
+                class='header-element'
+        >
+            {{element.name}}
+        </div>
+    </header>
 </template>
 
 <script>
     export default {
-        name: "app-header"
+        name: "app-header",
+        data: () => ({
+            elements: [
+                {
+                    name: 'Home',
+                    routeName: 'Home',
+                },
+                {
+                    name: 'Blog',
+                    routeName: 'Blog',
+                },
+                {
+                    name: 'About',
+                    routeName: 'About',
+                }
+            ]
+        })
     }
 </script>
 
