0159eca0497729ef6ae34424b3f83b5a0a4f8a5d
punkkk <punkrus18@gmail.com>
2020-02-13 14:03:22 +0300

    prettify: topic view

--- src/components/topics/topic-preview.vue
+++ src/components/topics/topic-preview.vue
@@ -1,21 +1,54 @@
 <template>
     <div class="topic-preview">
-        <div>{{topic.published}}</div>
-        <div>{{topic.title}}</div>
+        <div class="header">
+            <router-link class='title' :to="'sdas'">{{topic.title}}</router-link>
+            <div class="time">{{topic.published | publishedFormat}}</div>
+        </div>
         <div>{{topic.preview}}</div>
     </div>
 </template>
 
 <script>
+    import moment from 'moment';
+
     export default {
         name: "topic-preview",
         props: ['topic'],
+        filters: {
+            publishedFormat: (value) => {
+                return moment(value).format('DD.MM.YYYY HH:mm')
+            }
+        }
     }
 </script>
 
 <style scoped>
     .topic-preview {
-        width: 40rem;
+        width: 40%;
         margin: 10px 0;
+        padding-bottom: 10px;
+        border-bottom: 1px solid;
+        vertical-align: bottom;
+    }
+
+    .header {
+        display: flex;
+        justify-content: space-between;
+        padding-bottom: 10px;
+    }
+
+    .title {
+        color: black;
+        text-decoration: none;
+        font-size: 2.5rem;
+    }
+
+    .title:hover {
+        text-decoration: underline;
+    }
+
+    .time {
+        font-size: 1.25rem;
+        line-height: 3.5rem;
     }
 </style>
